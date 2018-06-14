75b40adcaccbafa3d41c974908a73b98977721b6
vipjue <1638153693@qq.com>
2018-06-14 10:24:50 +0800

    第三次修改李萍代码

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var oLi = document.getElementsByTagName("li");
 for(var i=0;i<oLi.length;i++){
     oLi[i].onclick = function(){
-        console.log("q")
+        console.log("q");
+        console.log(1);
     }
 }
