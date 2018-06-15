74d04e54d8a564b06bef5c93a034c9fb03966c71
userLp <2123422930@qq.com>
2018-06-15 08:40:47 +0800

    在分支2修改了wenjian

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var oLi = document.getElementsByTagName("li");
+var oBox = document.getElementsByClassName("box");
 for(var i=0;i<oLi.length;i++){
     oLi[i].onclick = function(){
         console.log("q");
         console.log(1);
+        for(var i=0;i<oBox.length;i++){
+            oBox[i].style.display="none";
+        }
+        oBox[this.index].style.display="block";
     }
 }
