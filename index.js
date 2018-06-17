7c782456863149bdc51bcff4cb9a48cabbd8a7b5
userLp <2123422930@qq.com>
2018-06-17 19:28:32 +0800

    添加了ol

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var oP = document.getElementById("options");
-var oLi = document.getElementById("list");
+var oL = document.getElementById("list");
 var oLi = oL.getElementsByTagName("li")
 var oLi2 = oP.getElementsByTagName("li");
 for(var i=0;i<oLi.length;i++){
     oLi[i].index = i;
-    for(var k=0;k<oLi2.length;k++){
-    console.log(1)
+    oLi[i].onclick=function(){
+        for(var j=0;j<oLi2.length;j++){
+            oLi[i].className="";
+             oLi2[j].style.display="none";
+        }
+        oLi[this.index].className="active";
+        oLi2[this.index].style.display='block'
     }
 }
